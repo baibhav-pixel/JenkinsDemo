373ceba88a439c175353581503772cf072c1fe97
baibhav-pixel <baibhav.sinha6@gmail.com>
2025-04-10 20:50:45 +0530

    Second commit

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,11 @@ public class Calculator
 		return a-b;
 	}
 
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+
 	public static void main(String[] args)
 	{
 		Calculator c = new Calculator();
