d4fa4000befa6d47a6f2c762fa1511137fcc73a2
baibhav-pixel <baibhav.sinha6@gmail.com>
2025-04-10 19:56:17 +0530

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator
+{
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int subtract(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args)
+	{
+		Calculator c = new Calculator();
+		System.out.println(c.add(5,6));
+		System.out.println(c.subtract(11,4));
+	}
+}
